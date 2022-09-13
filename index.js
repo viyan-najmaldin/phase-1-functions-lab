00e5f1a642aaf87e4581dbdf56c584fee9932e0c
viyan-najmaldin <najmadinviyan@gmail.com>
2022-09-13 22:24:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    if (someValue < 42) {
+      return 42 - someValue;
+    } else {
+      return someValue - 42;
+    }
+  }
+  console.log(distanceFromHqInBlocks(34));
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+  function distanceTravelledInFeet(a, b) {
+    if (b > a) {
+      return (b - a) * 264;
+    } else return (a - b) * 264;
+  }
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    }
+    if (distance >= 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance >= 2000 && distance <= 2500) {
+      return 25;
+    } else if (distance > 2500) {
+      return "cannot travel that far";
+    }
+  }
